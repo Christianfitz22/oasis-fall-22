44b47adcbb26a756a494b9442782ff1761553227
aulin1 <audrey.youran.lin@gmail.com>
2022-10-16 12:36:35 -0400

    created dummy opponentAI class
    ^that

---
+++ oasis-fall-22/Assets/Scripts/opponentAI.cs
@@ -0,0 +1,16 @@
+class opponentAI : playerStats{
+
+    /**
+    The constructor for opponentAI.
+    */
+    public opponentAI() : base(){
+        //bleh
+    }
+
+    /**
+    Used for the opponent to decide on the move.
+    */
+    private decideMove(){
+        //man
+    }
+}
